572638c2bed0f4fb8bcdf48850de85233d98aff8
ashiq <ashiq@dotserp.com>
2026-07-03 11:03:46 +0530

    Remove dist from repository

--- dist/routes/job.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const job_controller_1 = require("../controllers/job.controller");
-const jobRouter = (0, express_1.Router)();
-const controller = new job_controller_1.JobController();
-jobRouter.route('/').post(controller.create);
-exports.default = jobRouter;
